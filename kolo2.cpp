f73b9182b8129dca4b946bbeaebcc5a5e44291a9
Mateusz Banaszkiewicz <mat.banaszkiewicz@gmail.com>
2011-06-03 11:37:40 +0200

    dodano moje dane i gitignore

--- kolo2.cpp
+++ kolo2.cpp
@@ -4,7 +4,7 @@ using namespace std;
 const unsigned stala = 1000;
 
 int main(int argc, char **argv) {
-  cout<<"Imie Nazwisko, grupa(sepcjalnosc), nr. indeksu"<<endl;
+  cout<<"Mateusz Banaszkiewicz, aplikacja biznesowe i bazy danych, 101 249"<<endl;
   return 0;
 }
 
